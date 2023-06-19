0bba41d7c30540aa6c200850a63c840fbce4cd5d
eugeneas3 <eugenesoftengr@gmail.com>
2023-06-19 04:49:57 -0700

    Using betty method

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * A programme that uses puts instead of printf.
+ * A programme that does not return (0).
+ * The main function is wher the code is written
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
