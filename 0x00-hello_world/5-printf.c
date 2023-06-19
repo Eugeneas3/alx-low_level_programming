a16db785305e4eb0e45433a08fdf74d992941b63
eugeneas3 <eugenesoftengr@gmail.com>
2023-06-19 05:12:06 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - A programme that uses puts instead of printf
+ *Return: 0 (Success)  A programme that does not return (0)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
